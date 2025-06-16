7e68f643742d5535237a8cb5b6f0d8d73778499a
NazgulSkola <d.hyvnar.st@spseiostrava.cz>
2025-06-16 21:53:13 +0200

    Add files via upload

--- db/project_trip_app.sql
+++ db/project_trip_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Počítač: 127.0.0.1
--- Vytvořeno: Stř 09. dub 2025, 21:39
+-- Vytvořeno: Pon 16. čen 2025, 21:51
 -- Verze serveru: 10.4.32-MariaDB
 -- Verze PHP: 8.0.30
 
@@ -33,6 +33,15 @@ CREATE TABLE `difficulty` (
   `Multiplier` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
 
+--
+-- Vypisuji data pro tabulku `difficulty`
+--
+
+INSERT INTO `difficulty` (`ID_difficulty`, `Name_diff`, `Multiplier`) VALUES
+(1, 'Lehké', 1),
+(2, 'Střední', 1.5),
+(3, 'Těžké', 2);
+
 -- --------------------------------------------------------
 
 --
@@ -60,6 +69,13 @@ CREATE TABLE `trip` (
   `Creator` int(11) NOT NULL,
   `Difficulty` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
+
+--
+-- Vypisuji data pro tabulku `trip`
+--
+
+INSERT INTO `trip` (`ID_trip`, `Name_trip`, `Length`, `Time`, `Creator`, `Difficulty`) VALUES
+(1, 'Zkouska', 12.5, '03:20:00', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -98,7 +114,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`ID_user`, `Username`, `Email`, `Password`, `City`, `Country`, `Birthdate`, `Points`, `Rank`) VALUES
-(1, 'admin', 'admin@admin.cz', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'Ostrava', 'Česká Republika', '1999-01-01', 0, 0);
+(1, 'admin', 'admin@admin.cz', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'Ostrava', 'Česká Republika', '1999-01-01', 0, 0),
+(3, 'Davdik', 'd.hyvnar.st@spseiostrava.cz', '163d638cef03b7bdf9cd3115398375420e4b1119d41aad82783f3064d06ef3c6', '', 'Česká Republika', '2007-04-24', 0, 0);
 
 --
 -- Indexy pro exportované tabulky
@@ -120,19 +137,23 @@ ALTER TABLE `rank`
 -- Indexy pro tabulku `trip`
 --
 ALTER TABLE `trip`
-  ADD PRIMARY KEY (`ID_trip`);
+  ADD PRIMARY KEY (`ID_trip`),
+  ADD KEY `Difficulty` (`Difficulty`);
 
 --
 -- Indexy pro tabulku `trip_history`
 --
 ALTER TABLE `trip_history`
-  ADD PRIMARY KEY (`ID_th`);
+  ADD PRIMARY KEY (`ID_th`),
+  ADD KEY `ID_user` (`ID_user`),
+  ADD KEY `ID_trip` (`ID_trip`);
 
 --
 -- Indexy pro tabulku `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`ID_user`);
+  ADD PRIMARY KEY (`ID_user`),
+  ADD KEY `Rank` (`Rank`);
 
 --
 -- AUTO_INCREMENT pro tabulky
@@ -142,7 +163,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pro tabulku `difficulty`
 --
 ALTER TABLE `difficulty`
-  MODIFY `ID_difficulty` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_difficulty` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT pro tabulku `rank`
@@ -154,7 +175,7 @@ ALTER TABLE `rank`
 -- AUTO_INCREMENT pro tabulku `trip`
 --
 ALTER TABLE `trip`
-  MODIFY `ID_trip` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_trip` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pro tabulku `trip_history`
@@ -166,7 +187,24 @@ ALTER TABLE `trip_history`
 -- AUTO_INCREMENT pro tabulku `user`
 --
 ALTER TABLE `user`
-  MODIFY `ID_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ID_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Omezení pro exportované tabulky
+--
+
+--
+-- Omezení pro tabulku `trip`
+--
+ALTER TABLE `trip`
+  ADD CONSTRAINT `trip_ibfk_1` FOREIGN KEY (`Difficulty`) REFERENCES `difficulty` (`ID_difficulty`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Omezení pro tabulku `trip_history`
+--
+ALTER TABLE `trip_history`
+  ADD CONSTRAINT `trip_history_ibfk_1` FOREIGN KEY (`ID_trip`) REFERENCES `trip` (`ID_trip`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `trip_history_ibfk_2` FOREIGN KEY (`ID_user`) REFERENCES `user` (`ID_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
